d52373e6fe7569972744cd565a5abc09fea2039a
ahmadshobirin <ahmadshobirin.dev@gmail.com>
2020-06-19 00:07:12 +0700

    add saldo keuangan View,call keuanganView in MainClass

--- src/pengolahanuangpribadi/PengolahanUangPribadi.java
+++ src/pengolahanuangpribadi/PengolahanUangPribadi.java
@@ -15,7 +15,9 @@ public class PengolahanUangPribadi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        KeuanganView KeuanganView = new KeuanganView();
+        
+        KeuanganView.setVisible(true);
     }
     
 }
